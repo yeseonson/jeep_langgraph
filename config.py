8e52f568a70d881818e1871356e84a20d32660bf
yeseonson <ysson96@gmail.com>
2025-05-30 13:07:26 +0900

    chore: config에 VALKEY 추가, Openai client 추가

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 
@@ -11,3 +12,7 @@ OPENSEARCH_PORT = os.getenv('OPENSEARCH_PORT')
 GPT_4O_MINI_MODEL_ID = os.getenv('GPT_4O_MINI_MODEL_ID')
 PRODUCT_INDEX_NAME = "jeep_product"
 KNOWLEDGE_INDEX_NAME = "jeep_knowledge"
+VALKEY_HOST = os.getenv("VALKEY_HOST")
+VALKEY_PORT = os.getenv("VALKEY_PORT")
+VALKEY_PASSWORD = os.getenv("VALKEY_PASSWORD")
+OPENAI_CLIENT = OpenAI(api_key=OPENAI_API_KEY)
