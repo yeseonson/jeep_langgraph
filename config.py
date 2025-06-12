5096e616f2bc5c33a014f731d3261205251526ea
yeseonson <ysson96@gmail.com>
2025-06-12 13:17:45 +0900

    chore: 모델별로 모델ID 추가

--- config.py
+++ config.py
@@ -4,12 +4,13 @@ from openai import OpenAI
 
 load_dotenv()
 
-MODEL_ID = os.getenv('MODEL_ID')
 EMBEDDING_MODEL_ID = os.getenv('EMBEDDING_MODEL_ID')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 OPENSEARCH_HOST = os.getenv('OPENSEARCH_HOST')
 OPENSEARCH_PORT = os.getenv('OPENSEARCH_PORT')
 GPT_4O_MINI_MODEL_ID = os.getenv('GPT_4O_MINI_MODEL_ID')
+QWEN3_4B_MODEL_ID = os.getenv('QWEN3_4B_MODEL_ID')
+HYPERCLOVA_3B_MODEL_ID = os.getenv('HYPERCLOVA_3B_MODEL_ID')
 PRODUCT_INDEX_NAME = "jeep_product"
 KNOWLEDGE_INDEX_NAME = "jeep_knowledge"
 VALKEY_HOST = os.getenv("VALKEY_HOST")
