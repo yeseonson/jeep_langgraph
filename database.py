aaf84a3aac1b75760b4de22e24c51737225e904a
yeseonson <ysson96@gmail.com>
2025-06-19 11:14:08 +0900

    feat: 대화 이력 S3에 저장하는 로직 구현현

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
-import valkey
-
+import redis
+import boto3
+import json
 from opensearchpy import OpenSearch
-from config import OPENSEARCH_HOST, OPENSEARCH_PORT, VALKEY_HOST, VALKEY_PORT, VALKEY_PASSWORD
+from datetime import datetime, timedelta
+from typing import Dict, Any, Optional
+from config import OPENSEARCH_HOST, OPENSEARCH_PORT, VALKEY_HOST, VALKEY_PORT, VALKEY_PASSWORD, JEEP_S3_BUCKET
 from logger import logger
 
 
@@ -25,16 +28,66 @@ def opensearch_client():
 def valkey_client():
     logger.info("Attempting to connect to OpenSearch client: ")
 
+    if not VALKEY_HOST or not VALKEY_PORT:
+        raise ValueError("VALKEY_HOST and VALKEY_PORT must be set")
+
     try:
-        client = valkey.Valkey(
-            host=VALKEY_HOST, 
-            port=VALKEY_PORT, 
+        client = redis.Redis(
+            host=str(VALKEY_HOST), 
+            port=int(VALKEY_PORT), 
             db=0, 
             password=VALKEY_PASSWORD
         )
+        client.ping()
         logger.info("Successfully connected to Valkey client")
         return client
     
     except Exception as e:
         logger.error(f"Failed to connect to Valkey client: {e}")
         raise
+
+def s3_client():
+    if not JEEP_S3_BUCKET:
+        raise ValueError("JEEP_S3_BUCKET must be set")
+        
+    try:
+        client = boto3.client('s3')
+        client.head_bucket(Bucket=JEEP_S3_BUCKET)
+        logger.info(f"Bucket '{JEEP_S3_BUCKET}' exists and is accessible.")
+        return client
+    except Exception as e:
+        logger.error(f"Failed to connect to S3 client: {e}")
+        raise
+
+async def load_from_s3(session_id: str) -> Optional[Dict[str, Any]]:
+    """S3에서 대화 로드"""
+    try:
+        client = s3_client()
+        if not client:
+            return None
+            
+        for days_back in range(7):
+            date = datetime.now() - timedelta(days=days_back)
+            s3_key = f"chat_archives/{date.year}/{date.month:02d}/{date.day:02d}/{session_id}.json"
+
+            try:
+                response = client.get_object(Bucket=JEEP_S3_BUCKET, key=s3_key)
+                return json.loads(response['Body'].read().decode('utf-8'))
+            except client.exceptions.NoSuchKey:
+                continue
+    except Exception as e:
+        logger.error(f"S3 로드 실패: {e}")
+        
+    return None
+
+def s3_client_put_object(data):
+    try:
+        client = boto3.client('s3')
+        client.put_object(
+            Bucket=JEEP_S3_BUCKET,
+            Body=json.dumps(data.__dict__, ensure_ascii=False, indent=2),
+            ContentType='application/json'
+        )
+
+    except Exception as e:
+        logger.error(f"Failed to put object to S3: {e}")
