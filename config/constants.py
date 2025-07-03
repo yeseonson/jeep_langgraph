53ac32e6387834dfa2af9bccad530ee47ee90dd8
yeseonson <ysson96@gmail.com>
2025-07-03 14:42:12 +0900

    fix: update PRODUCT_TOP_K constant from 10 to 5

--- config/constants.py
+++ config/constants.py
@@ -1,4 +1,4 @@
-PRODUCT_TOP_K = 10
+PRODUCT_TOP_K = 5
 KNOWLEDGE_TOP_K = 5
 same_manufacturer_query = "AND recManufacturer.name_en = baseManufacturer.name_en"
 different_manufacturer_query = "AND recManufacturer.name_en <> baseManufacturer.name_en"
